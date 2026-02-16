a6df1671e1c945ec884e4f4ab51ff71b0b3d0169
K-nito <135321055+K-nito@users.noreply.github.com>
2026-02-16 13:38:54 +0530

    Refactor Tokenizer.c for improved structure and clarity

--- Tokenizer.c
+++ Tokenizer.c
@@ -1,177 +1,248 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 200
+
+FILE *fp;
+int row = 1, col = 0;
+
+typedef struct{
+    char lexeme[100];
+    char type[50];
+    int row;
+    int col;
+}Token;
 
 typedef struct{
     char name[50];
-    char datatype[20];
+    char datatype[50];
 }Symbol;
 
-typedef struct{
-    char fname[50];
-    Symbol table[100];
-    int count;
-}FunctionTable;
+Symbol globalTable[MAX];
+Symbol localTable[20][MAX];
 
-FILE *fp;
+int gCount = 0;
+int lCount[20] = {0};
+int funcIndex = -1;
 
-Symbol globalTable[100];
-int gCount=0;
+char currentDatatype[20] = "";
+char currentFunction[20] = "";
 
-FunctionTable functions[50];
-int fCount=0;
-
-int isAlpha(char c){return (c>='a'&&c<='z')||(c>='A'&&c<='Z');}
-int isDigit(char c){return c>='0'&&c<='9';}
-int isAlnum(char c){return isAlpha(c)||isDigit(c);}
-
-int isDatatype(char *s){
-    char *d[]={"int","float","double","char","void",NULL};
-    for(int i=0;d[i];i++)
-        if(!strcmp(d[i],s)) return 1;
+int isLetter(char c){
+    if(c>='a' && c<='z') return 1;
+    if(c>='A' && c<='Z') return 1;
     return 0;
 }
 
-int isKeyword(char *s){
-    char *k[]={"if","else","while","for","return","break","continue",NULL};
-    for(int i=0;k[i];i++)
-        if(!strcmp(k[i],s)) return 1;
+int isDigit(char c){
+    if(c>='0' && c<='9') return 1;
     return 0;
 }
 
-void addGlobal(char *name,char *type){
-    strcpy(globalTable[gCount].name,name);
-    strcpy(globalTable[gCount].datatype,type);
-    gCount++;
+int isKeyword(char *str){
+    char *kw[]={"if","else","while","for","return","break","continue"};
+    for(int i=0;i<7;i++)
+        if(strcmp(str,kw[i])==0) return 1;
+    return 0;
 }
 
-void createFunction(char *name){
-    strcpy(functions[fCount].fname,name);
-    functions[fCount].count=0;
-    fCount++;
+int isDatatype(char *str){
+    char *dt[]={"int","float","char","double","void"};
+    for(int i=0;i<5;i++)
+        if(strcmp(str,dt[i])==0) return 1;
+    return 0;
 }
 
-void addLocal(char *fname,char *name,char *type){
-    for(int i=0;i<fCount;i++){
-        if(!strcmp(functions[i].fname,fname)){
-            int c=functions[i].count;
-            strcpy(functions[i].table[c].name,name);
-            strcpy(functions[i].table[c].datatype,type);
-            functions[i].count++;
+int exists(Symbol table[],int count,char *name){
+    for(int i=0;i<count;i++)
+        if(strcmp(table[i].name,name)==0)
+            return 1;
+    return 0;
+}
+
+void addGlobal(char *name,char *datatype){
+    if(!exists(globalTable,gCount,name)){
+        strcpy(globalTable[gCount].name,name);
+        strcpy(globalTable[gCount].datatype,datatype);
+        gCount++;
+    }
+}
+
+void addLocal(char *name,char *datatype){
+    if(funcIndex==-1) return;
+    if(!exists(localTable[funcIndex],lCount[funcIndex],name)){
+        strcpy(localTable[funcIndex][lCount[funcIndex]].name,name);
+        strcpy(localTable[funcIndex][lCount[funcIndex]].datatype,datatype);
+        lCount[funcIndex]++;
+    }
+}
+
+Token getNextToken(){
+    Token t;
+    char c,buf[100];
+    int i;
+
+    while((c=fgetc(fp))!=EOF){
+
+        col++;
+
+        if(c=='\n'){
+            row++;
+            col=0;
+            continue;
+        }
+
+        if(c==' '||c=='\t')
+            continue;
+
+        if(c=='#'){
+            while((c=fgetc(fp))!='\n' && c!=EOF);
+            row++;
+            col=0;
+            continue;
+        }
+
+        t.row=row;
+        t.col=col;
+
+        if(c=='"'){
+            i=0;
+            buf[i++]=c;
+            while((c=fgetc(fp))!='"' && c!=EOF){
+                buf[i++]=c;
+                col++;
+            }
+            buf[i++]='"';
+            buf[i]='\0';
+            strcpy(t.lexeme,buf);
+            strcpy(t.type,"STRING_LITERAL");
+            return t;
+        }
+
+        if(isLetter(c) || c=='_'){
+            i=0;
+            buf[i++]=c;
+            while(isLetter(c=fgetc(fp)) || isDigit(c) || c=='_'){
+                buf[i++]=c;
+                col++;
+            }
+            buf[i]='\0';
+            ungetc(c,fp);
+
+            strcpy(t.lexeme,buf);
+
+            if(isDatatype(buf))
+                strcpy(t.type,"DATATYPE");
+            else if(isKeyword(buf))
+                strcpy(t.type,"KEYWORD");
+            else
+                strcpy(t.type,"IDENTIFIER");
+
+            return t;
+        }
+
+        if(isDigit(c)){
+            i=0;
+            buf[i++]=c;
+            while(isDigit(c=fgetc(fp))){
+                buf[i++]=c;
+                col++;
+            }
+            buf[i]='\0';
+            ungetc(c,fp);
+            strcpy(t.lexeme,buf);
+            strcpy(t.type,"NUMBER");
+            return t;
+        }
+
+        char next=fgetc(fp);
+        char op[3];
+        op[0]=c;
+        op[1]=next;
+        op[2]='\0';
+
+        if(strcmp(op,"==")==0 || strcmp(op,"!=")==0 ||
+           strcmp(op,"<=")==0 || strcmp(op,">=")==0 ||
+           strcmp(op,"&&")==0 || strcmp(op,"||")==0){
+            strcpy(t.lexeme,op);
+            strcpy(t.type,"OPERATOR");
+            col++;
+            return t;
+        }
+
+        ungetc(next,fp);
+
+        if(c=='+'||c=='-'||c=='*'||c=='/'||c=='%'||
+           c=='='||c=='<'||c=='>'||c=='!'){
+            t.lexeme[0]=c;
+            t.lexeme[1]='\0';
+            strcpy(t.type,"OPERATOR");
+            return t;
+        }
+
+        if(c=='('||c==')'||c=='{'||c=='}'||c==';'||c==','){
+            t.lexeme[0]=c;
+            t.lexeme[1]='\0';
+            strcpy(t.type,"SYMBOL");
+            return t;
         }
     }
-}
 
-void skipString(){
-    char c;
-    while((c=fgetc(fp))!='"' && c!=EOF);
-}
-
-void skipPreprocessor(){
-    char c;
-    while((c=fgetc(fp))!='\n' && c!=EOF);
-}
-
-void skipComment(){
-    char c,p=0;
-    while((c=fgetc(fp))!=EOF){
-        if(p=='*' && c=='/') break;
-        p=c;
-    }
+    strcpy(t.type,"EOF");
+    return t;
 }
 
 int main(){
-    char c,word[100],currentType[20]="";
-    char currentFunction[50]="";
-    int insideFunction=0;
-    int i;
 
-    fp=fopen("pptest.c","r");
-    if(!fp) return 1;
+    Token t;
+    fp=fopen("input.c","r");
 
-    while((c=fgetc(fp))!=EOF){
+    if(!fp){
+        printf("File not found\n");
+        return 0;
+    }
 
-        if(c=='#'){ skipPreprocessor(); continue; }
+    printf("TOKENS\n\n");
 
-        if(c=='"'){ skipString(); continue; }
+    while(strcmp((t=getNextToken()).type,"EOF")!=0){
 
-        if(c=='/'){
-            char n=fgetc(fp);
-            if(n=='*'){ skipComment(); continue; }
-            if(n=='/'){ skipPreprocessor(); continue; }
-            ungetc(n,fp);
+        printf("Row:%d Col:%d Type:%-15s Lexeme:%s\n",
+               t.row,t.col,t.type,t.lexeme);
+
+        if(strcmp(t.type,"DATATYPE")==0){
+            strcpy(currentDatatype,t.lexeme);
         }
 
-        if(isAlpha(c)){
-            i=0;
-            word[i++]=c;
-            while(isAlnum(c=fgetc(fp))){ word[i++]=c; }
-            word[i]='\0';
-            ungetc(c,fp);
+        else if(strcmp(t.type,"IDENTIFIER")==0){
 
-            if(isDatatype(word)){
-                strcpy(currentType,word);
-                continue;
+            char next=fgetc(fp);
+            ungetc(next,fp);
+
+            if(next=='(' && strcmp(currentDatatype,"")!=0){
+                addGlobal(t.lexeme,currentDatatype);
+                funcIndex++;
+                strcpy(currentFunction,t.lexeme);
             }
-
-            if(isKeyword(word)){
-                continue;
+            else{
+                if(funcIndex==-1)
+                    addGlobal(t.lexeme,currentDatatype);
+                else
+                    addLocal(t.lexeme,currentDatatype);
             }
-
-            if(strlen(currentType)>0){
-                char next=fgetc(fp);
-
-                if(next=='('){
-                    addGlobal(word,currentType);
-                    createFunction(word);
-                    strcpy(currentFunction,word);
-                    insideFunction=1;
-
-                    while((c=fgetc(fp))!=')'){
-                        if(isAlpha(c)){
-                            i=0;
-                            word[i++]=c;
-                            while(isAlnum(c=fgetc(fp))) word[i++]=c;
-                            word[i]='\0';
-                            ungetc(c,fp);
-
-                            if(isDatatype(word))
-                                strcpy(currentType,word);
-                            else if(strlen(currentType)>0){
-                                addLocal(currentFunction,word,currentType);
-                                currentType[0]='\0';
-                            }
-                        }
-                    }
-                }
-                else{
-                    ungetc(next,fp);
-                    if(!insideFunction)
-                        addGlobal(word,currentType);
-                    else
-                        addLocal(currentFunction,word,currentType);
-                }
-                currentType[0]='\0';
-            }
-        }
-
-        if(c=='}'){
-            insideFunction=0;
-            currentFunction[0]='\0';
         }
     }
 
     printf("\nGLOBAL SYMBOL TABLE\n");
     printf("Name\tDatatype\n");
-    for(i=0;i<gCount;i++)
+    for(int i=0;i<gCount;i++)
         printf("%s\t%s\n",globalTable[i].name,globalTable[i].datatype);
 
-    for(i=0;i<fCount;i++){
-        printf("\nLOCAL SYMBOL TABLE FOR FUNCTION %s\n",functions[i].fname);
+    for(int i=0;i<=funcIndex;i++){
+        printf("\nLOCAL SYMBOL TABLE FOR FUNCTION %s\n",globalTable[i].name);
         printf("Name\tDatatype\n");
-        for(int j=0;j<functions[i].count;j++)
-            printf("%s\t%s\n",
-                functions[i].table[j].name,
-                functions[i].table[j].datatype);
+        for(int j=0;j<lCount[i];j++)
+            printf("%s\t%s\n",localTable[i][j].name,localTable[i][j].datatype);
     }
 
     fclose(fp);
